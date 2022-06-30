e2b4ecdfb0e7d6e5df413de7253ebef87a8c7004
JenAila <JenAilaLesbianRobotics@gmail.com>
2022-06-29 22:48:36 -0400

    updated webscraper to include URL (as text) and emails the user - hardcoded TO and FROM emails.

---
+++ Django_Project/WebScrape/emailing_user.py
@@ -0,0 +1,52 @@
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email.mime.text import MIMEText
+from email.utils import COMMASPACE, formatdate
+from email import encoders
+import smtplib, ssl
+
+
+def email(send_to, url,subject):
+    send_from = 'donationsecondchance@gmail.com'
+    send_to = 'donationsecondchance@gmail.com'
+    # send_to = ['surbhiwahie@gmail.com','jenailalesbianrobotics@gmail.com', 'akanksha.bhattachan@gmail.com']
+    # url = ''
+    # subject = 'TEST email: Hello from second chance'  ## whatever subjectline you need to send
+    message_contents = """ 
+    <html>
+        <body>
+            <p><font color = "#404041">
+            Hi,
+            <br>
+            <br>
+            We are from Second chance. 
+            <br>
+            This is a test email.
+            <br>
+            <font face = "monospace">
+            <br>
+            <br>
+            Thanks,
+            <br>
+            Second Chance
+            </font></font></p>
+        </body>
+    </html>
+        """
+    server = 'smtp.gmail.com'
+    port = 587
+    username = 'donationsecondchance@gmail.com' ## this will change if we are creating a new emailID
+    password = 'oetvzabtkufklzax' ## we will get this after 2 factor authentication
+    isTls = True
+    msg = MIMEMultipart()
+    msg['From'] = send_from
+    msg['To'] = ','.join(send_to)
+    msg['Date'] = formatdate(localtime=True)
+    msg['Subject'] = subject
+    msg.attach(MIMEText(message_contents, "html"))
+    smtp = smtplib.SMTP(server, port)
+    if isTls:
+        smtp.starttls()
+        smtp.login(username, password)
+        smtp.sendmail(send_from, send_to, msg.as_string())
+        smtp.quit()
---
+++ Django_Project/WebScrape/webscrapping_kojojo.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from bs4 import BeautifulSoup as bs
+from emailing_user import email
+import requests
+import pandas as pd
+import re
+
+url = 'http://127.0.0.1:8000/'
+page = requests.get(url)
+soup = bs(page.text, 'html.parser')
+# print(soup.prettify())
+
+dfs = pd.read_html(page.text)
+df = dfs[0]
+
+#clean price field and convert to float
+df["Price"] = df["Price"].str.replace("$","", regex=True)
+df["Price_float"] = df["Price"].str.replace(",","", regex=True).astype('float')
+
+
+#convert all postal codes and categories into lower case
+postal_codes = ["M1B","M1C","M1E","M1G","M1H","M1J","M1K","M1L","M1M","M1N","M1P","M1R","M1S","M1T","M1V","M1W","M1X","M4A","M4B","M4C","M4E"]
+postals = []
+cat_lower = []
+for p in postal_codes:
+    postals.append(p.lower())
+
+category = ['bed', 'table', 'chair', 'sofa', 'dresser','tv']
+for cat in category:
+    cat_lower.append(cat.lower())
+
+postal_codes = postals
+category = cat_lower
+
+#First reducing the dataframe to search for item: Only look at items that are below a certain price
+df_price_match = df[df.Price_float < 50.0]
+df_price_match.loc[:,'Location'] = df_price_match.Location.str.lower()
+df_price_match.loc[:,'Name'] = df_price_match.Name.str.lower()
+
+#Capture key field values and search for an item within a postal code
+for i, post in enumerate(df_price_match.Name[1:3]): #sample
+    Email_This_Person = False
+    kojojo_price = df_price_match.iloc[i][2]
+    kojojo_location = df_price_match.iloc[i][3]
+    kojojo_item = df_price_match.iloc[i][1]
+    kojojo_email = df_price_match.iloc[i][4] 
+    kojojo_location = kojojo_location.replace(',', "")
+    kojojo_location = kojojo_location.replace('-', "")
+    kojojo_url = df_price_match.iloc[i][5] 
+
+    for item in category:
+        if(re.search(item, kojojo_item)):
+            
+            for i in kojojo_location.split(' '):
+                if(i =="scarborough" or i in postal_codes):
+                    Email_This_Person = True
+    if Email_This_Person:
+        print(f"\n\nEmail This person @{kojojo_email}! They are selling {kojojo_item} for {kojojo_price} at {kojojo_location}")
+        email(kojojo_email, kojojo_url, kojojo_item)
+        print("--")
+
+
+        
